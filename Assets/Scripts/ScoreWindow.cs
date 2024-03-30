347c75f5044265e8d1e6410692607b52a78f0632
QueenLinlay <leejusti217@gmail.com>
2024-03-29 20:09:45 -0400

    Update: Added HighScore to be seen during play

--- Assets/Scripts/ScoreWindow.cs
+++ Assets/Scripts/ScoreWindow.cs
@@ -7,14 +7,17 @@ using TMPro;
 public class ScoreWindow : MonoBehaviour
 {
     private TextMeshProUGUI scoreText;
+    private TextMeshProUGUI highText;
 
     private void Awake()
     {
         scoreText = transform.Find("scoreText").GetComponent<TextMeshProUGUI>();
+        highText = transform.Find("HighScoreNum").GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
         scoreText.text = Level.GetInstance().GetPipesPassedCount().ToString();
+        highText.text = GameOverWindow.NewHigh.ToString();
     }
 }
